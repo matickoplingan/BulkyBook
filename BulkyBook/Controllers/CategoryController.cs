e9a73c701652109949b3eac17530aceea4032b0f
matickoplingan <yoga.herawan89@gmail.com>
2022-02-15 14:46:34 +0700

    initial commit

---
+++ BulkyBook/Controllers/CategoryController.cs
@@ -0,0 +1,106 @@
+﻿using BulkyBook.Data;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db; 
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> _obj = _db.Categories;
+            return View(_obj);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0){
+                return NotFound();
+            }
+
+            var objCategory = _db.Categories.Find(id);
+
+            if (objCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(objCategory);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if(obj.DisplayOrder.ToString() == obj.Name)
+            {
+                ModelState.AddModelError("DisplayOrder", "Display Order Name tidak boleh sama dengan nama");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            var objCategory = _db.Categories.Find(id);
+
+            if(objCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(objCategory);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteAction(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
